b1e10a9751279af7bafec61a847af17c7d29581a
Moe <marvel451@aol.com>
2018-02-14 23:59:08 -0800

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,74 @@
-function getInfo () {
+document.getElementById('getData').addEventListener('click', getMarvelData);
+document.getElementById('clearData').addEventListener('click', clearBoard);
 
-var user = document.getElementById("inputHere").value
-var comment = document.getElementById("inputHere2").value
-
-var newUser = document.createTextNode("Username: " + user)
-var newComment = document.createTextNode("Comment: " + comment)
-
-var newList = document.createElement("li")
-newList.className = "coolclass"
-
-var newScreenName = document.createElement('p')
-newScreenName.appendChild(newUser)
-
-var newScreenComment = document.createElement("p")
-newScreenComment.appendChild(newComment)
-
-newList.appendChild(newScreenName)
-newList.appendChild(newScreenComment)
-
-
-document.getElementById("sweetlist").appendChild(newList)
-
+function clearBoard() {
+    document.getElementById("result").classList.add("hider")
 }
+ 
+  function getMarvelData(){
+    
+  let dateOne = document.getElementById('dateFirst').value,  
+   dateTwo = document.getElementById('dateSecond').value;
 
-function deleteInfo () {
-	var remover = document.getElementById('sweetlist')
-		remover.removeChild(remover.childNodes[0]);
-	}
+  fetch('https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=' + dateOne + '-01-01%2C' + dateTwo +'-01-02&characters=1009187&limit=20&apikey=cb6378a5a3f12f56c38a8325fbfa0dcb')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
 
+      // Examines the data in the response
+      response.json().then(function(data) {
 
-function getDate() {
-var today = new Date()
-var newPost = today.toDateString()
-var newScreenDate = document.createTextNode(newPost)
+        function createNode(element) {
+          return document.createElement(element); 
+          // Creates whatever element you pass as parameters
+        }
 
+        function append(parent, element) {
+          return parent.appendChild(element);
+           // Appends the second parameter(element) to the first
+        }
 
-document.getElementById("sweetlist").appendChild(newScreenDate)
+            let container = document.getElementById('result');
+
+                let marvelTitle = data.data.results
+
+                 // Map through the results and run code for each
+                    return marvelTitle.map(function(marvel) { 
+ 
+                  // Create 3 elements
+                     let div = createNode('div'),
+                     img = createNode('img'),
+                     par = createNode('p');
+
+                     par.classList.add("card");
+ 
+                     img.src = marvel.thumbnail.path + '/standard_fantastic.jpg';
+                 
+
+                      append(div, img);
+                      append(div, par);
+                      append(container, div);
+                    })
+
+                output = marvelTitle
+
+            
+ 
+                     
+ 
+            
+           
+    
+
+       });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
 
 }
