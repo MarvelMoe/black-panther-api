b0ccc6d64a6a9409ad7741b1f08c3bf4aeea5e01
Moe <marvel451@aol.com>
2018-02-15 17:15:32 -0800

    First Commital

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,71 @@
 document.getElementById('getData').addEventListener('click', getMarvelData);
 document.getElementById('clearData').addEventListener('click', clearBoard);
 
-function clearBoard() {
-    document.getElementById("result").classList.add("hider")
-}
- 
-  function getMarvelData(){
-    
-  let dateOne = document.getElementById('dateFirst').value,  
-   dateTwo = document.getElementById('dateSecond').value;
 
-  fetch('https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=' + dateOne + '-01-01%2C' + dateTwo +'-01-02&characters=1009187&limit=20&apikey=cb6378a5a3f12f56c38a8325fbfa0dcb')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+function getMarvelData() {
 
-      // Examines the data in the response
-      response.json().then(function(data) {
+    let dateOne = document.getElementById('dateFirst').value,
+        dateTwo = document.getElementById('dateSecond').value;
 
-        function createNode(element) {
-          return document.createElement(element); 
-          // Creates whatever element you pass as parameters
-        }
+    fetch('https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=' + dateOne + '-01-01%2C' + dateTwo + '-01-02&characters=1009187&limit=50&apikey=cb6378a5a3f12f56c38a8325fbfa0dcb')
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
 
-        function append(parent, element) {
-          return parent.appendChild(element);
-           // Appends the second parameter(element) to the first
-        }
+                // Examines the data in the response
+                response.json().then(function(data) {
 
-            let container = document.getElementById('result');
+                    function createNode(element) {
+                        // Creates whatever element you pass as parameters
+                        return document.createElement(element);
+                    }
 
-                let marvelTitle = data.data.results
+                    function append(parent, element) {
+                        // Appends the second parameter(element) to the first
+                        return parent.appendChild(element);
+                    }
 
-                 // Map through the results and run code for each
-                    return marvelTitle.map(function(marvel) { 
- 
-                  // Create 3 elements
-                     let div = createNode('div'),
-                     img = createNode('img'),
-                     par = createNode('p');
+                    let container = document.getElementById('result');
 
-                     par.classList.add("card");
- 
-                     img.src = marvel.thumbnail.path + '/standard_fantastic.jpg';
-                 
+                    let marvelData = data.data.results
 
-                      append(div, img);
-                      append(div, par);
-                      append(container, div);
+                    // Map through the results and run code for each
+                    return marvelData.map(function(marvel) {
+
+                        // Create 3 elements
+                        let div = createNode('div'),
+                            img = createNode('img'),
+                            par = createNode('p');
+
+                        // Attach img and title of comic to elements  
+                        img.src = marvel.thumbnail.path + '/standard_fantastic.jpg';
+                        par.innerHTML = `${marvel.title}`;
+                        div.classList.add("card");
+
+                        append(div, img);
+                        append(div, par);
+                        append(container, div);
                     })
 
-                output = marvelTitle
+                    output = marvelData
 
-            
- 
-                     
- 
-            
-           
-    
 
-       });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
 
+
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+
+}
+
+function clearBoard() {
+  // Removes everything from the screen
+    document.getElementById("result").classList.add("hider")
 }
