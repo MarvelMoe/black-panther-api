35e73364072a78f8f3ed130caa58acb2c841f4f9
Moe <marvel451@aol.com>
2018-02-22 23:46:51 -0800

    Added loading icon
    Added icon to show user feedback and removed clearing data button. Also added dropping animation of results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
+
+
 function getMarvelData() {
 
     let dateOne = document.getElementById('dateFirst').value,
         dateTwo = document.getElementById('dateSecond').value;
+
 
     fetch('https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=' + dateOne + '-01-01%2C' + dateTwo + '-01-02&characters=1009187&limit=50&apikey=cb6378a5a3f12f56c38a8325fbfa0dcb')
         .then(function(response) {
@@ -13,6 +16,7 @@ function getMarvelData() {
 
                 // Examines the data in the response
                 response.json().then(function(data) {
+               
 
                     function createNode(element) {
                         // Creates whatever element you pass as parameters
@@ -23,31 +27,38 @@ function getMarvelData() {
                         // Appends the second parameter(element) to the first
                         return parent.appendChild(element);
                     }
-                      
+                                  
                     // Add results to container in the end
                     let container = document.getElementById('result'),
                         marvelData = data.data.results
 
-                    // Map through the results and run code for each
-                    return marvelData.map(function(marvel) {
+                        // Add animation to reveal data
+                          container.classList.add("revealer");
 
-                        // Create 3 elements
-                        let div = createNode('div'),
-                            img = createNode('img'),
-                            par = createNode('p');
+                        // Map through the results and run code for each
+                        return marvelData.map(function(marvel) {
 
-                        // Attach img and title of comic to elements  
-                        img.src = marvel.thumbnail.path + '/standard_fantastic.jpg';
-                        par.innerHTML = `${marvel.title}`;
-                        div.classList.add("card");
-                        append(div, img);
-                        append(div, par);
-                        append(container, div);
-                    })
+                            // Create 3 elements
+                            let div = createNode('div'),
+                                img = createNode('img'),
+                                par = createNode('p');
+
+                            // Attach img and title of comic to elements  
+                            img.src = marvel.thumbnail.path + '/standard_fantastic.jpg';
+                            par.innerHTML = `${marvel.title}`;
+                            div.classList.add("card");
+
+                            append(div, img);
+                            append(div, par);
+                            append(container, div);
+                        })
                   
                  // Spit out results
                     output = marvelData
                 })
+
+
+               
             })
         
         .catch(function(err) {
@@ -56,10 +67,15 @@ function getMarvelData() {
 
 }
 
-function clearBoard() {
-  // Removes everything from the screen
-    document.getElementById("result").classList.add("hider")
+
+
+function loadingData() {
+ // Adds loader before data arrives
+document.getElementById('icon').style.display = 'block';
 }
 
-document.getElementById('getData').addEventListener('click', getMarvelData);
-document.getElementById('clearData').addEventListener('click', clearBoard);
+document.getElementById('getData').addEventListener('click', function () {
+ getMarvelData();
+ loadingData();
+});
+ 
